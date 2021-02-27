9deb8b9ae532a15b9b2296ad493529e1c39e7682
Valerij Ivashchenko <likemusicdev@gmail.com>
2021-02-27 15:55:38 +0300

    export local vars.

--- inc/maposmatic-frontend.sh
+++ inc/maposmatic-frontend.sh
@@ -27,16 +27,16 @@ mkdir -p logs rendering/results media
 # copy config files
 cp $FILEDIR/config-files/config.py scripts/config.py
 
-BBOX_MAXIMUM_LENGTH_IN_METERS=${BBOX_MAXIMUM_LENGTH_IN_METERS:-20000}
+export BBOX_MAXIMUM_LENGTH_IN_METERS=${BBOX_MAXIMUM_LENGTH_IN_METERS:-20000}
 
-PAPER_MIN_WITH_MM=${PAPER_MIN_WITH_MM:-100}
-PAPER_MAX_WITH_MM=${PAPER_MAX_WITH_MM:-2000}
+export PAPER_MIN_WITH_MM=${PAPER_MIN_WITH_MM:-100}
+export PAPER_MAX_WITH_MM=${PAPER_MAX_WITH_MM:-2000}
 
-PAPER_MIN_HEIGHT_MM=${PAPER_MIN_HEIGHT_MM:-100}
-PAPER_MAX_HEIGHT_MM=${PAPER_MAX_HEIGHT_MM:-2000}
+export PAPER_MIN_HEIGHT_MM=${PAPER_MIN_HEIGHT_MM:-100}
+export PAPER_MAX_HEIGHT_MM=${PAPER_MAX_HEIGHT_MM:-2000}
 
-MAPOSMATIC_FORK_URL=${MAPOSMATIC_FORK_URL:-'https://github.com/hholzgra/maposmatic'}
-OCITYSMAP_FORK_URL=${OCITYSMAP_FORK_URL:-'https://githib.com/hholzgra/ocitysmap'}
+export MAPOSMATIC_FORK_URL=${MAPOSMATIC_FORK_URL:-'https://github.com/hholzgra/maposmatic'}
+export OCITYSMAP_FORK_URL=${OCITYSMAP_FORK_URL:-'https://githib.com/hholzgra/ocitysmap'}
 
 cat $FILEDIR/config-files/settings_local.py | envsubst > www/settings_local.py
 cp $FILEDIR/config-files/maposmatic.wsgi www/maposmatic.wsgi
