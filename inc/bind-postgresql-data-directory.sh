73b36a46a9a40a1e2bac999cccf05786444eceaf
Valerij Ivashchenko <likemusicdev@gmail.com>
2021-03-03 13:49:22 +0300

    Bind PostgreSql data directory to host file without LVM.

--- inc/bind-postgresql-data-directory.sh
+++ inc/bind-postgresql-data-directory.sh
@@ -3,12 +3,20 @@ then
   rm /disk/lvm0.img
 fi
 
+mkdir -p /var/lib/postgresql/12
+
 truncate -s ${POSTGRESQL_DATA_DISK_SIZE:-6T} /disk/lvm0.img
 losetup /dev/loop0 /disk/lvm0.img
-pvcreate /dev/loop0
-vgcreate vg0 /dev/loop0
-lvcreate -n lv0 -l 100%FREE vg0
-mkfs.ext4 /dev/vg0/lv0
 
-mkdir -p /var/lib/postgresql/12
-mount /dev/vg0/lv0 /var/lib/postgresql/12
+# NO LVM
+echo ",,83,," | sudo sfdisk /dev/loop0
+sudo partx --update /dev/loop0
+mkfs.ext4 /dev/loop0p1
+mount /dev/loop0p1 /var/lib/postgresql/12
+
+# LVM
+#pvcreate /dev/loop0
+#vgcreate vg0 /dev/loop0
+#lvcreate -n lv0 -l 100%FREE vg0
+#mkfs.ext4 /dev/vg0/lv0
+#mount /dev/vg0/lv0 /var/lib/postgresql/12
