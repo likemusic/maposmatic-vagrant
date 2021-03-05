f463c0bbab59b6fe34e5220eb138da9bd79f8caf
Valerij Ivashchenko <likemusicdev@gmail.com>
2021-03-05 15:46:40 +0300

    Add progress info during process in elevation-data.sh.

--- inc/elevation-data.sh
+++ inc/elevation-data.sh
@@ -2,16 +2,10 @@
 
 cd /home/maposmatic
 
-
-
-# read SRTM 90m zone name -> area mapping table
-echo "Importing SRTM zone database"
-sudo -u maposmatic psql gis < /vagrant/files/database/db_dumps/srtm_zones.sql > /dev/null
-
 mkdir -p elevation-data
 cd elevation-data
 
-echo "Downloading SRTM arcive files"
+echo "Downloading SRTM archive files"
 
 mkdir -p srtm-data
 cd srtm-data
@@ -20,6 +14,7 @@ mkdir -p $CACHEDIR/srtm-data
 
 # extract bounding box data in bash array format
 bbox=$(cat /home/maposmatic/bounds/bbox.bash)
+#todo: stop process bbox if not exists
 
 # create actaul bounding box bash array
 eval b=$bbox
@@ -28,43 +23,60 @@ eval b=$bbox
 polygon="POLYGON((${b[1]} ${b[0]}, ${b[1]} ${b[2]}, ${b[3]} ${b[2]}, ${b[3]} ${b[0]}, ${b[1]} ${b[0]}))"
 
 # now download all zones the import bounding box overlaps with
-for zone in $(psql gis --tuples-only --command="select zone from srtm_zones where ST_INTERSECTS(way, ST_GeomFromText('$polygon', 4326))")
-do
-    CACHEFILE=$CACHEDIR/srtm-data/$zone.zip
+let cnt=$(psql gis --tuples-only --command="select zone from srtm_zones where ST_INTERSECTS(way, ST_GeomFromText('$polygon', 4326))" | wc -l)
+let i=1;
+for zone in $(psql gis --tuples-only --command="select zone from srtm_zones where ST_INTERSECTS(way, ST_GeomFromText('$polygon', 4326))"); do
+  let percents=$i*100/$cnt
+
+  echo " $percents% ($i/$cnt): zone $zone ... "
+
+  CACHEFILE=$CACHEDIR/srtm-data/$zone.zip
+
+  if [[ "$zone" == *\/* ]]; then
+    SRTM_URL=http://viewfinderpanoramas.org/$zone.zip
+  else
     SRTM_URL=http://viewfinderpanoramas.org/dem3/$zone.zip
-    if ! test -f $CACHEFILE
-    then
-	echo "  downloading zone $zone"
-	wget -q -O $CACHEFILE $SRTM_URL
-    fi
-    unzip -q $CACHEFILE
+  fi
+
+  if ! test -f $CACHEFILE; then
+    echo "  downloading ... "
+    curl $SRTM_URL --create-dirs -o $CACHEFILE
+  fi
+
+  echo "  unzip ..."
+  unzip -q $CACHEFILE
+  i=$(($i+1))
 done
 
 cd ..
-
-
-
 
 echo "SRTM hillshading for PisteMap"
 
 mkdir -p srtm
 cd srtm
 
-for file in $(find /home/maposmatic/elevation-data/srtm-data/ -name "*.hgt" | sort)
-do
-    base=$(basename $file .hgt)
-    echo "  processing $base"
-    gdal_translate -q -of GTiff -co "TILED=YES" -a_srs "+proj=latlong" $file ${base}_adapted.tif
+let cnt=$(find /home/maposmatic/elevation-data/srtm-data/ -name "*.hgt" | sort | wc -l)
+let i=1;
 
-    gdalwarp -q -multi -of GTiff -co "TILED=YES" -srcnodata 32767 -t_srs "+proj=merc +ellps=sphere +R=6378137 +a=6378137 +units=m" -rcs -order 3 -tr 30 30 -multi ${base}_adapted.tif ${base}_warped.tif
+for file in $(find /home/maposmatic/elevation-data/srtm-data/ -name "*.hgt" | sort); do
+  base=$(basename $file .hgt)
 
-    gdaldem hillshade -q ${base}_warped.tif ${base}_hillshade.tif
+  let percents=$i*100/$cnt
+
+  echo " $percents% ($i/$cnt): $base ... "
+
+  echo "  gdal_translate ... "
+  gdal_translate -q -of GTiff -co "TILED=YES" -a_srs "+proj=latlong" $file ${base}_adapted.tif
+
+  echo "  gdalwarp ... "
+  gdalwarp -q -multi -of GTiff -co "TILED=YES" -srcnodata 32767 -t_srs "+proj=merc +ellps=sphere +R=6378137 +a=6378137 +units=m" -rcs -order 3 -tr 30 30 -multi ${base}_adapted.tif ${base}_warped.tif
+
+  echo "  gdaldem ... "
+  gdaldem hillshade -q ${base}_warped.tif ${base}_hillshade.tif
+  i=$(($i+1))
 done
 
 cd ..
-
-
-
 
 echo "DEM data for OpenTopoMap"
 mkdir -p dem
@@ -74,10 +86,15 @@ cd dem
 cp /vagrant/files/relief_color_text_file.txt .
 
 # fill empty spaces
-for file in $(find /home/maposmatic/elevation-data/srtm-data -name "*.hgt" | sort)
-do
-  echo "  processing "$(basename $file .hgt)
+let cnt=$(find /home/maposmatic/elevation-data/srtm-data/ -name "*.hgt" | sort | wc -l)
+let i=1
+for file in $(find /home/maposmatic/elevation-data/srtm-data -name "*.hgt" | sort); do
+  let percents=$i*100/$cnt
+  base=$(basename $file .hgt)
+
+  echo " $percents% ($i/$cnt): $base ... "
   gdal_fillnodata.py -q $file $(basename $file).tif
+  i=$(($i+1))
 done
 
 # merge all elevation data into one single large tiled file
@@ -89,9 +106,11 @@ ln -s raw.tif dem_srtm.tiff
 
 # convert to google mercator projection
 interpolation=cubicspline
-for r in 90 500 1000 5000
-do
-  echo "interpolation $r"
+let cnt=4
+let i=1
+for r in 90 500 1000 5000; do
+  let percents=$i*100/$cnt
+  echo " $percents% ($i/$cnt): interpolation $r ... "
   gdalwarp -co BIGTIFF=YES -co TILED=YES -co COMPRESS=LZW -co PREDICTOR=2 -t_srs "+proj=merc +ellps=sphere +R=6378137 +a=6378137 +units=m" -r $interpolation -tr $r $r raw.tif warp-$r.tif -q
   interpolation=bilinear
 done
@@ -99,9 +118,9 @@ done
 # create colored reliefs for low zoom levels
 echo "low color reliefs"
 gdaldem color-relief -co COMPRESS=LZW -co PREDICTOR=2 -alpha warp-5000.tif relief_color_text_file.txt relief-5000.tif -q
-gdaldem color-relief -co COMPRESS=LZW -co PREDICTOR=2 -alpha warp-500.tif  relief_color_text_file.txt relief-500.tif -q
+gdaldem color-relief -co COMPRESS=LZW -co PREDICTOR=2 -alpha warp-500.tif relief_color_text_file.txt relief-500.tif -q
 
-# create hillshading 
+# create hillshading
 echo "hillshading"
 gdaldem hillshade -z 7 -compute_edges -co COMPRESS=JPEG warp-5000.tif hillshade-5000.tif -q
 gdaldem hillshade -z 7 -compute_edges -co BIGTIFF=YES -co TILED=YES -co COMPRESS=JPEG warp-1000.tif hillshade-1000.tif -q
@@ -109,15 +128,10 @@ gdaldem hillshade -z 4 -compute_edges -co BIGTIFF=YES -co TILED=YES -co COMPRESS
 gdaldem hillshade -z 7 -combined -compute_edges -co compress=lzw -co predictor=2 -co bigtiff=yes warp-90.tif hillshade-90.tif -q
 # TODO: not used? gdal_translate -co compress=JPEG -co bigtiff=yes -co tiled=yes hillshade-90.tif hillshade-90-jpeg.tif -q
 
-# set up countours database and table schema
-echo "create contour db"
-sudo -u maposmatic psql --quiet gis < /vagrant/files/database/db_dumps/contours_schema.sql 
-
 # create contours shapefile and imports its data into the database
 echo "gdal_contour"
 gdal_contour -i 10 -a ele warp-90.tif contour.shp -q
 echo "shp2pgsql"
-shp2pgsql -a -g way -s 3857 contour.shp contours | psql --quiet contours 
+shp2pgsql -a -g way -s 3857 contour.shp contours | psql --quiet contours
 
 cd ..
-
