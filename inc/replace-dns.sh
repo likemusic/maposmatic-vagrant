3f843dcba1443558fcb61fa178e0c37cc3d5ca2f
Valerij Ivashchenko <likemusicdev@gmail.com>
2021-03-07 13:09:53 +0300

    Fix DNS string.

--- inc/replace-dns.sh
+++ inc/replace-dns.sh
@@ -2,5 +2,5 @@
 
 echo "*** Replace DNS ***"
 
-sed -i.bak -e "s/^DNS=.*$/DNS=${DNS:-'8.8.8.8 8.8.4.4'}/g" /etc/systemd/resolved.conf
+sed -i.bak -e "s/^DNS=.*$/DNS=${DNS:-8.8.8.8 8.8.4.4}/g" /etc/systemd/resolved.conf
 systemctl restart systemd-resolved.service
